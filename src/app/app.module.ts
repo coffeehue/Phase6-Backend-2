8d063641cf3e58332cd5a7fcf3cbd985eafd1bc0
Saahil <65531623+coffeehue@users.noreply.github.com>
2022-05-31 10:13:08 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule } from "@angular/common/http";
+import { AppComponent } from './app.component';
+import { ListFoodComponent } from './components/list-food/list-food.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListFoodComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/food.ts
@@ -0,0 +1,8 @@
+export class Food {
+    id: number;
+    foodItem: string;
+    price: string;
+    category: string;
+    cuisine: number;
+    foodImage: string;
+}
